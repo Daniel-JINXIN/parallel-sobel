0af379c3c3e5880442c862feeabb8bc5f2847ea1
Yoann Ricordel <leyaude@gmail.com>
2012-12-02 18:39:06 +0100

    Merge bidon

--- sobel/implem_cuda.cu
+++ sobel/implem_cuda.cu
@@ -62,11 +62,16 @@ __device__ inline unsigned char greyscale_value(struct pixel *pImage, uint32_t p
 
 
 //XXX We do redundant computation between threads, can be improved with shared memory
+<<<<<<< Updated upstream
 /* pxNum is the index of the central pixel for the convolution in the pImage array */
 __device__ inline int32_t convolution_by_3(struct pixel *pImage, kernel_t kernel,
                                            uint32_t pxNum, uint32_t width, uint32_t height)
+=======
+__device__ inline int16_t convolution_by_3(unsigned char *pImage, kernel_t kernel,
+                                           uint32_t baseIndex, uint32_t width, uint32_t height)
+>>>>>>> Stashed changes
 {
-        int32_t grad = 0;
+        int16_t grad = 0;
         /* Line below */
         grad += kernel[0][0] * greyscale_value(pImage, pxNum + width + 1);
         grad += kernel[0][1] * greyscale_value(pImage, pxNum + width);
@@ -87,12 +92,19 @@ __device__ inline int32_t convolution_by_3(struct pixel *pImage, kernel_t kernel
 
 
 
+<<<<<<< Updated upstream
 
 #if USE_SHARED_MEM
 __global__ void sobel_unnorm_kernel(struct pixel *pInImageData, uint16_t *pOutImageData,
                                     uint32_t width, uint32_t height)
 {
         extern __shared__ struct pixel locImgPart[];
+=======
+__global__ void sobel_unnorm_kernel(unsigned char *pInImageData, uint16_t *pOutImageData,
+                                    uint32_t width, uint32_t height)
+{
+        /*extern __shared__ unsigned char localImagePart[];*/
+>>>>>>> Stashed changes
 
         uint32_t x = blockIdx.x * blockDim.x + threadIdx.x;
         uint32_t y = blockIdx.y * blockDim.y + threadIdx.y;
@@ -173,6 +185,7 @@ __global__ void sobel_unnorm_kernel(struct pixel *pInImageData, uint16_t *pOutIm
         int32_t gradX = convolution_by_3(locImgPart, kernelX, baseIndex, localBlockWidth, localBlockHeight);
         int32_t gradY = convolution_by_3(locImgPart, kernelY, baseIndex, localBlockWidth, localBlockHeight);
 
+<<<<<<< Updated upstream
         float gradX_asFloat = static_cast<float>(gradX);
         float gradY_asFloat = static_cast<float>(gradY);
         uint16_t gradNorm = static_cast<uint32_t>(
@@ -212,13 +225,24 @@ __global__ void sobel_unnorm_kernel(struct pixel *pInImageData, uint16_t *pOutIm
         float gradX_asFloat = static_cast<float>(gradX);
         float gradY_asFloat = static_cast<float>(gradY);
         uint16_t gradNorm = static_cast<uint32_t>(
+=======
+        int16_t gradX = convolution_by_3(pInImageData, kernelX, baseIndex, width, height);
+        int16_t gradY = convolution_by_3(pInImageData, kernelY, baseIndex, width, height);
+
+        float gradX_asFloat = static_cast<float>(gradX);
+        float gradY_asFloat = static_cast<float>(gradY);
+        uint16_t gradNorm = static_cast<uint16_t>(
+>>>>>>> Stashed changes
                                 sqrt(gradX_asFloat*gradX_asFloat + gradY_asFloat*gradY_asFloat));
 
         pOutImageData[pxNum] = gradNorm;
 }
+<<<<<<< Updated upstream
 
 
 #endif
+=======
+>>>>>>> Stashed changes
 
 
 void log_time(FILE *logFile, char *testName, uint32_t size, double t, int numThreads)
@@ -277,8 +301,6 @@ int sobel(struct image *const pInImage, struct image *pOutImage)
         int blockWidth = width / 256 + (width % 256 == 0 ? 0 : 1);
         int blockHeight = height / 256 + (height % 256 == 0 ? 0 : 1);
 
-        printf("%d, %d\n", blockWidth, blockHeight);
-
         dim3 threadsPerBlock(blockWidth, blockHeight);
 #endif
 
@@ -314,6 +336,7 @@ int sobel(struct image *const pInImage, struct image *pOutImage)
         /*check_mem(pOutImage->data);*/
 
         /* And launch the kernel */
+<<<<<<< Updated upstream
         // local block size: including a border
 #if USE_SHARED_MEM
         uint32_t localMemSize = ((blockWidth + 2) * (blockHeight + 2)) * sizeof(struct pixel);
@@ -322,6 +345,11 @@ int sobel(struct image *const pInImage, struct image *pOutImage)
         sobel_unnorm_kernel <<< nBlocks, threadsPerBlock >>> (inImageDevice, outImageDevice, width, height);
 #endif
 
+=======
+        // image block, PLUS the borders
+        /*uint32_t localMemSize = (blockWidth + 1) * (blockHeight + 1);*/
+        sobel_unnorm_kernel <<< nBlocks, threadsPerBlock/*, localMemSize*/ >>> (inImageDevice, outImageDevice, width, height);
+>>>>>>> Stashed changes
 
         ret = cudaMemcpy(pUnNormalizedOut, outImageDevice,
                          width * height * sizeof(uint16_t), cudaMemcpyDeviceToHost);
@@ -348,7 +376,10 @@ int sobel(struct image *const pInImage, struct image *pOutImage)
         }
 
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
         cudaFree(inImageDevice);
         cudaFree(outImageDevice);
         return 0;
